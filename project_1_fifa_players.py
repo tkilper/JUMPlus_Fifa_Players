c2a5d83751b97ed20b392ae4dbc454336ebb108c
Tristan Kilper <tkilperedu@gmail.com>
2021-11-18 13:24:55 -0500

    Question 6 cleaned and finished

--- project_1_fifa_players.py
+++ project_1_fifa_players.py
@@ -3,10 +3,11 @@
 
 import pandas as pd
 import numpy as np
+import seaborn as sns
 import matplotlib.pyplot as plt
 
 # Import dataset
-df = pd.read_csv('JUMPlus/Project1FifaPlayers/players_20.csv')
+df = pd.read_csv('JUMPlus/Project1FifaPlayers/JUMPlus_Fifa_Players/players_20.csv')
 
 # top 5 records
 print(df.head(5))
@@ -20,18 +21,15 @@ print(f'Number of rows in dataset: {len(df)}')
 print(f'Number of columns in dataset: {len(df.columns)}')
 
 # show top 10 number of players per country
-players_by_country = df.groupby(['nationality']).count().sort_values(['sofifa_id'], ascending=False)
+players_by_country = df.groupby(['nationality'], as_index=False).count().sort_values(['sofifa_id'], ascending=False)
 print(players_by_country.head(10))
-print(players_by_country.head(5).index)
 
 # bar chart of top 5 countries by players
-"""x_pos = players_by_country.head(5).index
+pbcbar = players_by_country[['nationality','sofifa_id']].head(5)
+pbcbar.columns = ['Nationality', 'Number of Players']
+sns.barplot(x='Nationality', y='Number of Players', data=pbcbar, color="green")
+plt.show()
 
-plt.bar(x_pos, players_by_country.head(5)['sofifa_id'], color='green')
-plt.xlabel("Nationality")
-plt.ylabel("Number of Players")
-plt.title("Top 5 Countries by Number of Players")
+# show top 5 players' short names and wages
 
-plt.xticks(x_pos, players_by_country.head(5)['sofifa_id'])
-
-plt.show()"""
+#
